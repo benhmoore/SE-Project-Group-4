676a0ad8a8a7f736f98a1a3a91a118ef64833eba
Ben Moore <benmoore@Bens-Mac-Studio.local>
2023-04-07 21:39:04 -0500

    Allowed cross-site scripting (bad idea in a production environment, but convenient for this project)

--- backend/django_project-pro/django_project/settings.py
+++ backend/django_project-pro/django_project/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'website'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +70,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
+]
 
 
 # Database
