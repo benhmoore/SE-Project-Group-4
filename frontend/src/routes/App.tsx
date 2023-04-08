16372fb56aa8fe0c5ee920e2e9be0426b4fa0323
Ben Moore <benmoore@Bens-Mac-Studio.local>
2023-04-07 21:20:51 -0500

    Improved code quality within cart interface.

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 
+import Axios from "axios";
+
 import Menu from "../components/menu/Menu";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
@@ -8,6 +10,20 @@ import Footer from "../components/Footer";
 export default function App() {
   const [authenticated, setAuthenticated] = useState(false);
   const [user, setUser] = useState({ username: "", token: "" });
+
+  Axios.post(
+    "https://72b573f8-1ce4-4cf7-97f9-84dd76c252a3.mock.pstmn.io/user/signin",
+    {
+      username: "bhm128",
+      password: "password",
+    }
+  )
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
   return (
     <div>
---
+++ frontend/src/utils/props/JSONCartItem.tsx
@@ -0,0 +1,7 @@
+export interface JSONCartItem {
+  productId: number;
+  cartItemId: number;
+  price: number;
+  name: string;
+  quantity: number;
+}
