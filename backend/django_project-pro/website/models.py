1cbb127e0e4c1e3e0d366c82eb3cb131a97bd314
Maxwell Lam <lam.maxwell2020@gmail.com>
2023-04-09 13:33:01 -0500

    Finished backend login

--- backend/django_project-pro/website/models.py
+++ backend/django_project-pro/website/models.py
@@ -10,3 +10,4 @@ class User(models.Model):
     address = models.CharField(max_length=255)
     balance = models.DecimalField(decimal_places=2, max_digits = 10)
     payment_method = models.CharField(max_length=255)
+    token_id = models.CharField(max_length=255)
