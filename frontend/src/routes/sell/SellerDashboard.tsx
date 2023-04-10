3d643d206efa19768eda4c3fd35b094d6ad40def
Ben Moore <ben@benhmoore.com>
2023-04-10 14:18:33 -0500

    Added initial seller dashboard UI.

--- frontend/src/routes/sell/SellerDashboard.tsx
+++ frontend/src/routes/sell/SellerDashboard.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import ProductManager from "../../components/product/ProductManager";
+import { BsPlusCircle } from "react-icons/bs";
+import { MdDashboard, MdEdit } from "react-icons/md";
 
 const SellerDashboard = () => {
   return (
@@ -14,13 +17,13 @@ const SellerDashboard = () => {
           <div className="card p-4">
             <div className="d-flex align-items-start">
               <div
-                className="nav flex-column nav-pills me-3"
+                className="nav flex-column nav-pills me-3 col-2"
                 id="v-pills-tab"
                 role="tablist"
                 aria-orientation="vertical"
               >
                 <button
-                  className="nav-link active"
+                  className="nav-link text-start active"
                   id="v-pills-home-tab"
                   data-bs-toggle="pill"
                   data-bs-target="#v-pills-home"
@@ -29,10 +32,11 @@ const SellerDashboard = () => {
                   aria-controls="v-pills-home"
                   aria-selected="true"
                 >
+                  <MdDashboard className="me-2 mb-1" />
                   Dashboard
                 </button>
                 <button
-                  className="nav-link"
+                  className="nav-link text-start"
                   id="v-pills-profile-tab"
                   data-bs-toggle="pill"
                   data-bs-target="#v-pills-profile"
@@ -41,10 +45,11 @@ const SellerDashboard = () => {
                   aria-controls="v-pills-profile"
                   aria-selected="false"
                 >
+                  <BsPlusCircle className="me-2 mb-1" />
                   Add Product
                 </button>
                 <button
-                  className="nav-link"
+                  className="nav-link text-start"
                   id="v-pills-messages-tab"
                   data-bs-toggle="pill"
                   data-bs-target="#v-pills-messages"
@@ -53,10 +58,14 @@ const SellerDashboard = () => {
                   aria-controls="v-pills-messages"
                   aria-selected="false"
                 >
-                  Manage
+                  <MdEdit className="me-2 mb-1" />
+                  Manage Products
                 </button>
               </div>
-              <div className="tab-content" id="v-pills-tabContent">
+              <div
+                className="tab-content container-fluid p-3"
+                id="v-pills-tabContent"
+              >
                 <div
                   className="tab-pane show active"
                   id="v-pills-home"
@@ -92,7 +101,9 @@ const SellerDashboard = () => {
                   role="tabpanel"
                   aria-labelledby="v-pills-profile-tab"
                 >
-                  ...
+                  <h2>Add Product</h2>
+                  <p>Add a new product to the storefront.</p>
+                  <ProductManager />
                 </div>
                 <div
                   className="tab-pane"
@@ -100,7 +111,42 @@ const SellerDashboard = () => {
                   role="tabpanel"
                   aria-labelledby="v-pills-messages-tab"
                 >
-                  ...
+                  <h2>Manage Products</h2>
+                  <p>Manage your products.</p>
+                  <table className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Stock</th>
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Product 1</td>
+                        <td>$10</td>
+                        <td>10</td>
+                        <td>
+                          <button className="btn btn-primary">Edit</button>
+                          <button className="btn btn-danger ms-2">
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>Product 1</td>
+                        <td>$10</td>
+                        <td>10</td>
+                        <td>
+                          <button className="btn btn-primary">Edit</button>
+                          <button className="btn btn-danger ms-2">
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
