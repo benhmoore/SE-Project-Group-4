204f0bdab25b2bfc849242979bd913735fbe5e3a
Ben Moore <ben@benhmoore.com>
2023-04-14 16:09:46 -0500

    Updated get_orders to handle products that were deleted. Finished updates for seller routes.

--- backend/django_project-pro/website/forms.py
+++ backend/django_project-pro/website/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 
+
 class SigninForm(forms.Form):
     username = forms.CharField(label='username', max_length=100)
     password = forms.CharField(label='password', max_length=100)
+
 
 class accountForm(forms.Form):
     userRole = forms.IntegerField()
@@ -14,43 +16,51 @@ class accountForm(forms.Form):
     balance = forms.CharField(label='balance', max_length=10)
     payment_method = forms.CharField(label='payment_method', max_length=100)
 
+
 class deleteAccountForm(forms.Form):
     username = forms.CharField(label='username', max_length=100)
     password = forms.CharField(label='password', max_length=100)
+
 
 class updateAccountForm(forms.Form):
     token = forms.CharField(label='username', max_length=100)
     payment_method = forms.CharField(label='payment_method', max_length=100)
     address = forms.CharField(label='address', max_length=100)
 
+
 class AddProductForm(forms.Form):
-    category = forms.CharField(max_length=255)
+    # category = forms.CharField(max_length=255)
+    token = forms.CharField(max_length=255)
     name = forms.CharField(max_length=255)
     price = forms.DecimalField()
-    seller = forms.IntegerField()
     image_id = forms.CharField(max_length=255)
-    num_sales = forms.IntegerField()
     inventory = forms.IntegerField()
-    approval_status = forms.IntegerField()
     description = forms.CharField(max_length=255)
+
 
 class updateQuantity(forms.Form):
     id = forms.CharField(max_length=255)
     quantity = forms.IntegerField()
+
 
 class ReturnOrderForm(forms.Form):
     token = forms.CharField(max_length=255)
     order_id = forms.IntegerField()
 
 ######################################################################################
+
+
 class RemoveProductForm(forms.Form):
     product_id = forms.IntegerField()
+
 
 class ShoppingCartForm(forms.Form):
     token = forms.CharField(max_length=255)
 
+
 class AddToCartForm(forms.Form):
     item_id = forms.IntegerField()
+
 
 class RemoveFromCartForm(forms.Form):
     item_id = forms.IntegerField()
